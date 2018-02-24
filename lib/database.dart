a69f7652df24bd6e41178471bfc17f78c3b27ff5
Daniel Dadap <daniel@dadap.net>
2018-02-24 13:24:39 -0600

    Wire up remaining entry fields
    Store everything except the ID as strings for now; we can use more
    appropriate bespoke types later if need be.

--- lib/database.dart
+++ lib/database.dart
@@ -48,12 +48,43 @@ class WordDatabase {
 
 class WordDatabaseEntry {
   WordDatabaseEntry.fromXmlNode(xml.XmlNode node) {
+    id = int.parse(innerText(ofNode: node, withName: 'id'));
     entryName = innerText(ofNode: node, withName: 'entry_name');
+    partOfSpeech = innerText(ofNode: node, withName: 'part_of_speech');
     definition = innerText(ofNode: node, withName: 'definition');
+    definitionDE = innerText(ofNode: node, withName: 'definition_de');
+    synonyms = innerText(ofNode: node, withName: 'synonyms');
+    antonyms = innerText(ofNode: node, withName: 'antonyms');
+    seeAlso = innerText(ofNode: node, withName: 'see_also');
+    notes = innerText(ofNode: node, withName: 'notes');
+    notesDe = innerText(ofNode: node, withName: 'notes_de');
+    hiddenNotes = innerText(ofNode: node, withName: 'hidden_notes');
+    components = innerText(ofNode: node, withName: 'components');
+    examples = innerText(ofNode: node, withName: 'examples');
+    examplesDe = innerText(ofNode: node, withName: 'examples_de');
+    searchTags = innerText(ofNode: node, withName: 'search_tags');
+    searchTagsDe = innerText(ofNode: node, withName: 'search_tags_de');
+    source = innerText(ofNode: node, withName: 'source');
   }
 
+  int id;
   String entryName;
+  String partOfSpeech;
   String definition;
+  String definitionDE;
+  String synonyms;
+  String antonyms;
+  String seeAlso;
+  String notes;
+  String notesDe;
+  String hiddenNotes;
+  String components;
+  String examples;
+  String examplesDe;
+  String searchTags;
+  String searchTagsDe;
+  String source;
+
 
   static String innerText({xml.XmlNode ofNode, String withName}) {
     Iterable<xml.XmlNode> matching = ofNode.children.where((child) {
