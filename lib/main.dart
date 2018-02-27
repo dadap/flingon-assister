80c7cb845120bac6611a008f765c073f6e7bc353
Daniel Dadap <daniel@dadap.net>
2018-02-27 06:04:13 -0600

    Mimic the look of the original boQwI'
    Set theme colors to approximate the appearance of the original boQwI'.

--- lib/main.dart
+++ lib/main.dart
@@ -13,15 +13,11 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'boQwI\'',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        primarySwatch: Colors.red,
+        primaryColor: Colors.red[900],
+        accentColor: Colors.redAccent,
+        highlightColor: Colors.red,
       ),
       home: new MyHomePage(title: 'boQwI\''),
     );
